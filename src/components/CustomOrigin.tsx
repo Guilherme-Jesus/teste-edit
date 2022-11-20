d50b6b345564eb7b1f9bb001466f027c93365d2b
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2022-11-20 13:45:03 -0300

    [x] só falta salvar

--- src/components/CustomOrigin.tsx
+++ src/components/CustomOrigin.tsx
@@ -1,14 +1,27 @@
 import SortableTree, {
+  addNodeUnderParent,
+  changeNodeAtPath,
   getFlatDataFromTree,
   getTreeFromFlatData,
+  removeNodeAtPath,
+  toggleExpandedForAll,
 } from '@nosferatu500/react-sortable-tree'
 import '@nosferatu500/react-sortable-tree/style.css'
+import axios from 'axios'
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { Button, InputGroup } from 'react-bootstrap'
 import { IListBlocks } from '../types'
 
 const CustomOrigin = () => {
   const [blocks, setBlocks] = useState<IListBlocks[]>([])
+  const [blocksAux, setBlocksAux] = useState<IListBlocks[]>([])
+  const [searchString, setSearchString] = useState<string>('')
+  const [searchFocusIndex, setSearchFocusIndex] = useState<number>(0)
+
+  const handleSearchStringChange = useCallback((event: any) => {
+    setSearchString(event.target.value)
+  }, [])
 
   useEffect(() => {
     fetch('http://localhost:7010/blocks')
@@ -18,34 +31,214 @@ const CustomOrigin = () => {
 
   const someOnlineAdvice = {
     treeData: getTreeFromFlatData({
-      flatData: blocks.map((node) => ({ ...node, title: node.name })),
-      getKey: (node) => node.blockId, // resolve a node's key
-      getParentKey: (node) => node.blockParent, // resolve a node's parent's key
-      rootKey: '0', // The value of the parent key when there is no parent (i.e., at root level)
+      flatData: blocks.map((node) => ({
+        ...node,
+        title: node.name,
+      })),
+
+      getKey: (node) => node.blockId,
+      getParentKey: (node) => node.blockParent,
+      rootKey: '0',
     }),
   }
-
   const flatData = getFlatDataFromTree({
     treeData: someOnlineAdvice.treeData,
     getNodeKey: ({ node }) => node.blockId, // This ensures your "id" properties are exported in the path
-    ignoreCollapsed: false, // Makes sure you traverse every node in the tree, not just the visible ones
+    ignoreCollapsed: false,
   }).map(({ node, path }) => ({
-    id: node.blockId,
+    blockId: node.blockId,
     name: node.name,
-
-    // The last entry in the path is this node's key
-    // The second to last entry (accessed here) is the parent node's key
-    parent: path.length > 1 ? path[path.length - 2] : null,
+    abrv: node.abrv,
+    blockParent: path.length > 1 ? path[path.length - 2] : '0',
+    data: node.data,
+    date: node.date,
+    leafParent: node.leafParent,
   }))
-  console.log(flatData)
+
+  const expandAll = useCallback(() => {
+    const expanded = toggleExpandedForAll({
+      treeData: someOnlineAdvice.treeData,
+      expanded: true,
+    }) as IListBlocks[]
+    setBlocks(expanded)
+  }, [someOnlineAdvice.treeData])
+
+  const collapseAll = useCallback(() => {
+    const expanded = toggleExpandedForAll({
+      treeData: someOnlineAdvice.treeData,
+      expanded: false,
+    }) as IListBlocks[]
+    setBlocks(expanded)
+  }, [someOnlineAdvice.treeData])
+
+  const handleSave = useCallback(() => {
+    if (flatData.length > 0) {
+      axios
+        .put(`http://localhost:7010/blocks/${flatData[0].blockId}`, flatData)
+
+        .then((res) => {
+          setBlocksAux(res.data)
+          setBlocks(res.data)
+          expandAll()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }, [expandAll, flatData])
 
   const onChange = (treeData: IListBlocks[]) => {
     setBlocks(treeData)
+    setBlocksAux(treeData)
   }
+
+  const addNode = useCallback(
+    (path: number[]) => {
+      const newBlocks = addNodeUnderParent({
+        treeData: someOnlineAdvice.treeData,
+        parentKey: path[path.length - 1],
+        expandParent: true,
+        getNodeKey: ({ treeIndex }) => treeIndex,
+        newNode: {
+          blockId: Math.random().toString(36),
+          blockParent: path[path.length - 1],
+          name: 'Nova Área',
+          abrv: 'Editar Abreviação',
+        },
+        addAsFirstChild: true,
+      }).treeData as IListBlocks[]
+      setBlocks(newBlocks)
+      setBlocksAux(newBlocks)
+    },
+    [someOnlineAdvice.treeData],
+  )
+  const removeNode = useCallback(
+    (path: number[]) => {
+      const newBlocks = removeNodeAtPath({
+        treeData: someOnlineAdvice.treeData,
+        path,
+        getNodeKey: ({ treeIndex }) => treeIndex,
+      }) as IListBlocks[]
+      setBlocks(newBlocks)
+      setBlocksAux(newBlocks)
+    },
+    [someOnlineAdvice.treeData],
+  )
 
   return (
     <div style={{ height: 800, width: 800 }}>
-      <SortableTree treeData={someOnlineAdvice.treeData} onChange={onChange} />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: 10,
+        }}
+      >
+        {blocks.length > 0 && (
+          <div>
+            <Button onClick={expandAll}>Expandir</Button>
+            <Button onClick={collapseAll}>Recolher</Button>
+            <Button
+              onClick={() => {
+                handleSave()
+              }}
+            >
+              Save
+            </Button>
+          </div>
+        )}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={searchString}
+          onChange={handleSearchStringChange}
+          placeholder="Pesquisar"
+        />
+      </div>
+      <SortableTree
+        searchQuery={searchString}
+        searchFocusOffset={searchFocusIndex}
+        treeData={someOnlineAdvice.treeData}
+        onChange={onChange}
+        searchFinishCallback={(matches) =>
+          setSearchFocusIndex(
+            matches.length > 0 ? searchFocusIndex % matches.length : 0,
+          )
+        }
+        generateNodeProps={({ node, path }) => ({
+          buttons: [
+            <Button
+              key={node.id}
+              variant="primary"
+              onClick={() => {
+                addNode(path)
+              }}
+            >
+              Criar
+            </Button>,
+            <Button
+              key={node.id}
+              variant="secondary"
+              onClick={() => {
+                removeNode(path)
+              }}
+            >
+              Remover
+            </Button>,
+          ],
+          title: (
+            <InputGroup>
+              <input
+                type="text"
+                value={node.title}
+                onChange={(e) => {
+                  setBlocks((state) => {
+                    const newDaum = changeNodeAtPath({
+                      treeData: state,
+                      path,
+                      getNodeKey: ({ treeIndex }) => treeIndex,
+                      newNode: {
+                        ...node,
+                        name: e.target.value,
+                      },
+                    }) as IListBlocks[]
+                    setBlocksAux(newDaum)
+                    return newDaum
+                  })
+                }}
+              />
+              <input
+                type="text"
+                value={node.abrv}
+                onChange={(e) => {
+                  setBlocks((state) => {
+                    const newDaum = changeNodeAtPath({
+                      treeData: state,
+                      path,
+                      getNodeKey: ({ treeIndex }) => treeIndex,
+                      newNode: {
+                        ...node,
+                        abrv: e.target.value,
+                      },
+                    }) as IListBlocks[]
+                    setBlocksAux(newDaum)
+                    return newDaum
+                  })
+                }}
+              />
+            </InputGroup>
+          ),
+        })}
+      />
+      <ul>
+        {flatData.map(({ blockId, name, blockParent }) => (
+          <li key={blockId}>
+            id: {blockId}, name: {name}, parent: {blockParent || 'null'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
